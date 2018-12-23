ac35f35d1fc9477b0834561f617affd8b8ed46c7
Tom Hayosh <thayosh@gmail.com>
2018-12-23 08:12:09 -0600

    First unit test for GCD

---
+++ totatives-tdd.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class GCDTestCase(unittest.TestCase):
+    def test_gcd_for_a_few_selected_cases(self):
+        self.assertEqual(0, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
